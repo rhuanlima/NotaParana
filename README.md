316b9c6f8f8b12c991e31b33ee5d9fb6a110e109
Rhuan Lima <rhuan@rhuanlima.com.br>
2020-08-01 11:43:58 -0300

    Add Readme

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-# export PDF:
+# NOTA PARANÁ SCRIPT EXPORT
 
-for exprt a pdf version of NF we use a pdfkit and wkhtmltopdf, a pdfkit is a python package listed on requirements.txt but wkhtmltopdf you need to install using this link:
-https://github.com/JazzCore/python-pdfkit/wiki/Installing-wkhtmltopdf
+Script python desenvolvido para consulta e exportação dos ultimos 13 meses de notas fiscais do nota Paraná
+
+## How to use
+-  Criar arquivo .env com os parametros:
+    ```
+    user='CPF_SOMENTE_NUMEROS'
+    password='SENHA_DO_NOTA_PARANA'
+    ```
+- Instalar as dependencias
+    ```
+    pip install -r requirements.txt
+    ```
+- Instalar o pdfkit:
+    https://github.com/JazzCore/python-pdfkit/wiki/Installing-wkhtmltopdf
+
+- Executar o script:
+    ```
+    python main.py
+    ```
+
+
+## Results
+
+Após a execução é criado o csv ``` data_nf.csv ``` com a lista de notas com os valores:
+
+    nf_nr : Número da Nota Fiscal
+    dt_emissao : Timestamp da emissáo da nota
+    place_name : Nome do estabelecimento
+    vl_nf : Valor da nota fiscal
+    acess_key : Chave de acesso
+    qr_code_link : Link para a nota no site da Receita Federal
+
+Além disso o script exporta as páginas da Receita Federal usando o **pdfkit** no caminho **./nf_pdf**
+
+## TODO:
+- Formatar a base de exportação que atualmente esta somente como texto
+- Aprimorar a exportação de PDF pois em alguns casos o link não é fornecido pelo portal
--- main.py
+++ main.py
@@ -6,9 +6,18 @@ from time import sleep
 import pandas as pd
 import html5lib
 import bs4
+from datetime import datetime
 
 
 def get_value(element):
+    """Verifica se o elemento existe, se sim retorna seu valor text se não retorna None
+
+    Args:
+        element (xpath): Valor do xpath do elemento a ser verificado
+
+    Returns:
+        String: retorna o Valor do elemento selecionado ou None
+    """
     try:
         element_value = driver.find_element_by_xpath(element).text
     except:
@@ -18,7 +27,7 @@ def get_value(element):
 
 load_dotenv()
 
-
+get_value()
 driver = webdriver.Chrome('./chromedriver')
 
 url = 'https://notaparana.pr.gov.br/'
@@ -40,10 +49,10 @@ a_link = '/html/body/div[2]/div[2]/div/div[4]/form/fieldset/div[4]/table/tbody/t
 window_before = driver.window_handles[0]
 
 
+df = pd.DataFrame(columns=['nf_nr', 'dt_emissao',
+                           'place_name', 'vl_nf', 'acess_key', 'qr_code_link'])
 
-df = pd.DataFrame(columns=['nf_nr', 'dt_emissao', 'place_name', 'acess_key', 'qr_code_link'])
-
-for paginas in range(1, 12):
+for paginas in range(1, 13):
     nr_linhas = pd.read_html(driver.find_element_by_xpath(
         '/html/body/div[2]/div[2]/div/div[4]/form/fieldset/div[4]/table').get_attribute('outerHTML'))[0].shape[0]
     for nr_row in range(2, nr_linhas):
@@ -51,30 +60,38 @@ for paginas in range(1, 12):
         sleep(2)
         window_after = driver.window_handles[1]
         driver.switch_to.window(window_after)
+
         acess_key = driver.find_element_by_xpath(
             '/html/body/div[1]/fieldset/table/tbody/tr/td[1]/span').text
+
         place_name = driver.find_element_by_xpath(
             '/html/body/div[2]/div[1]/div/fieldset[2]/table/tbody/tr/td[2]/span').text
+
         nf_nr = driver.find_element_by_xpath(
             '/html/body/div[1]/fieldset/table/tbody/tr/td[2]/span').text
+
         dt_emissao = driver.find_element_by_xpath(
             '/html/body/div[2]/div[1]/div/fieldset[1]/table/tbody/tr/td[4]/span').text
+
+        vl_nf = driver.find_element_by_xpath(
+            '/html/body/div[2]/div[1]/div/fieldset[1]/table/tbody/tr/td[6]/span').text
+
         driver.find_element_by_xpath('/html/body/ul/li[8]').click()
 
         if get_value('/html/body/div[2]/div[7]/div/fieldset/fieldset[3]/table/tbody/tr[1]/td/span') != None:
             qr_code_link = get_value(
                 '/html/body/div[2]/div[7]/div/fieldset/fieldset[3]/table/tbody/tr[1]/td/span')
         elif get_value('/html/body/div[2]/div[7]/div/fieldset/fieldset[2]/table/tbody/tr[1]/td/span') != None:
-            qr_code = get_value(
+            qr_code_link = get_value(
                 '/html/body/div[2]/div[7]/div/fieldset/fieldset[2]/table/tbody/tr[1]/td/span')
         elif get_value('/html/body/div[2]/div[7]/div/fieldset/fieldset/table/tbody/tr[1]/td/span') != None:
-            qr_code = get_value(
+            qr_code_link = get_value(
                 '/html/body/div[2]/div[7]/div/fieldset/fieldset/table/tbody/tr[1]/td/span')
         else:
-            qr_code = 'ERROR'
+            qr_code_link = 'ERROR'
 
-        item = [nf_nr, dt_emissao, place_name, acess_key, qr_code_link]
-        print(item)
+        item = [nf_nr, dt_emissao, place_name, vl_nf, acess_key, qr_code_link]
+        print(item[:2])
         df.loc[len(df)] = item
         driver.close()
         driver.switch_to.window(window_before)
@@ -82,9 +99,16 @@ for paginas in range(1, 12):
         '/html/body/div[2]/div[2]/div/div[4]/form/fieldset/div[5]/button[1]').click()
     sleep(2)
 
-df.to_csv('data_nf.csv',sep=';')
-
-pdfkit.from_url('http://google.com', 'out.pdf')
-
-
+df.to_csv('data_nf.csv', sep=';')
+driver.find_element_by_xpath(
+    '/html/body/div[2]/div[1]/div/div[2]/div/a[2]').click()
+sleep(10)
 driver.close()
+
+for index, row in df.iterrows():
+    dt_nf = datetime.strptime(row['dt_emissao'][:-6], '%d/%m/%Y %H:%M:%S')
+    try:
+        pdfkit.from_url(row['qr_code_link'], './nf_pdf/nf_{}_{}_{}.pdf'.format(
+            dt_nf.strftime('%Y%m%d'), row['nf_nr'], row['place_name'][:10]))
+    except:
+        print(row)
